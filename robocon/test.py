75b30d3c3132c0467a419049acc478dadb1792ac
ordonese <108791046+ordonese@users.noreply.github.com>
2022-07-27 12:36:15 +0900

    Update test.py

--- robocon/test.py
+++ robocon/test.py
@@ -19,7 +19,6 @@ FREQ = 50               #サーボの設定
 GPIO.setmode(GPIO.BCM)              #GPIOのモードを"GPIO.BCM"に設定
 GPIO.setup(Trig, GPIO.OUT)          #GPIO27を出力モードに設定
 GPIO.setup(Echo, GPIO.IN)           #GPIO18を入力モードに設定
-servo = GPIO.PWM(PIN, FREQ)         #サーボの設定
 
 GPIO.setup(PIN, GPIO.OUT)
 servo = GPIO.PWM(PIN, FREQ)
