c179b04c4c72c538e32e7257caca78864e8279e8
ordonese <108791046+ordonese@users.noreply.github.com>
2022-08-14 15:54:19 +0900

    Update Servomotor.py

--- robocon_real/Servomotor.py
+++ robocon_real/Servomotor.py
@@ -1,1 +1,23 @@
+import RPi.GPIO as GPIO
+import time
 
+INTERVAL = 0.6
+PIN = 14
+FREQ = 50
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(PIN, GPIO.OUT)
+servo = GPIO.PWM(PIN, FREQ)
+
+#init
+servo.start(0.0)
+
+for i in range(2):
+  servo.ChangeDutyCycle(2.5)
+  time.sleep(INTERVAL)
+
+  servo.ChangeDutyCycle(16.75)
+  time.sleep(INTERVAL)
+  
+GPIO.cleanup()
