23561d010f3a13d4cf0f907e3d7826b63f523131
kb5884 <105502011+kb5884@users.noreply.github.com>
2022-08-20 15:39:05 +0900

    Create steppingmotor.py

---
+++ robocon_real/steppingmotor.py
@@ -0,0 +1,22 @@
+from signal import SIG_UNBLOCK
+import RPi.GPIO as GPIO             #GPIO用のモジュールをインポート
+import time                         #時間制御用のモジュールをインポート
+import sys                          #sysモジュールをインポート
+
+CW=17
+CCW=23
+
+#GPIOの設定
+GPIO.setmode(GPIO.BCM)              #GPIOのモードを"GPIO.BCM"に設定
+GPIO.setup(CW, GPIO.OUT)          #GPIO27を出力モードに設定
+GPIO.setup(CCW, GPIO.IN)           #GPIO18を入力モードに設定
+
+GPIO.output(CW, GPIO.HIGH)            #GPIO27の出力をHigh(3.3V)にする
+time.sleep(0.00001)                     #10μ秒間待つ
+GPIO.output(CW, GPIO.LOW)               #GPIO27の出力をLow(0V)にする
+
+print("finish")
+
+GPIO.cleanup()
+print("cleanupped")
+sys.exit()
