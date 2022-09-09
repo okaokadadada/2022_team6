12445b09742e4b4a5242ff64612e9be3ec38c0dd
kb5884 <105502011+kb5884@users.noreply.github.com>
2022-09-09 18:43:17 +0900

    Update awo.py

--- robocon_real/awo.py
+++ robocon_real/awo.py
@@ -2,7 +2,7 @@ from signal import SIG_UNBLOCK
 import RPi.GPIO as GPIO             #GPIO用のモジュールをインポート
 import time                         #時間制御用のモジュールをインポート
 import sys                          #sysモジュールをインポート
-
+GPIO.setwarnings(False)
 AWO=2
 
 GPIO.setmode(GPIO.BCM)
