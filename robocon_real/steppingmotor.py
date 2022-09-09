29212a3425a497abe0938ee22ce87faf7c304e5b
kb5884 <105502011+kb5884@users.noreply.github.com>
2022-09-09 18:56:51 +0900

    Update steppingmotor.py

--- robocon_real/steppingmotor.py
+++ robocon_real/steppingmotor.py
@@ -9,11 +9,12 @@ CCW=23
 #GPIOの設定
 GPIO.setmode(GPIO.BCM)              #GPIOのモードを"GPIO.BCM"に設定
 GPIO.setup(CW, GPIO.OUT)          #GPIO27を出力モードに設定
-GPIO.setup(CCW, GPIO.IN)           #GPIO18を入力モードに設定
+GPIO.setup(CCW, GPIO.OUT)           #GPIO18を入力モードに設定
 
 while True:
   try:
     
+    GPIO.output(CCW,GPIO.LOW)
     GPIO.output(CW, GPIO.HIGH)            #GPIO27の出力をHigh(3.3V)にする
     time.sleep(0.00001)                     #10μ秒間待つ
     GPIO.output(CW, GPIO.LOW)               #GPIO27の出力をLow(0V)にする
