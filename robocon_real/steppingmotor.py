21fa8aeb13f3773999db5f68cb31c73af80dd8c9
ordonese <108791046+ordonese@users.noreply.github.com>
2022-09-24 12:42:08 +0900

    Update steppingmotor.py

--- robocon_real/steppingmotor.py
+++ robocon_real/steppingmotor.py
@@ -22,17 +22,17 @@ while True:
     for i in range(1000):
       GPIO.output(CWp, GPIO.HIGH)
       GPIO.output(CWm, GPIO.LOW)             #CWをONに
-      time.sleep(0.03)
+      time.sleep(0.05)
       GPIO.output(CWp, GPIO.LOW)
       GPIO.output(CWm, GPIO.HIGH)            #CWをOFFに
-      time.sleep(0.04)
+      time.sleep(0.05)
     for i in range(1000):
       GPIO.output(CCWp, GPIO.HIGH)
       GPIO.output(CCWm, GPIO.LOW)            #CCWをONに
-      time.sleep(0.04)
+      time.sleep(0.05)
       GPIO.output(CCWp, GPIO.LOW)
       GPIO.output(CCWm, GPIO.HIGH)           #CCWをOFFに
-      time.sleep(0.04)
+      time.sleep(0.05)
     
   except KeyboardInterrupt:
     GPIO.cleanup()
