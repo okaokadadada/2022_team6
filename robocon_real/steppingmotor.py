192ec86b9c2d21ae3a1bb7e04ccfff0f97d581c4
ordonese <108791046+ordonese@users.noreply.github.com>
2022-09-24 12:02:53 +0900

    Update steppingmotor.py

--- robocon_real/steppingmotor.py
+++ robocon_real/steppingmotor.py
@@ -9,27 +9,27 @@ CCWp=23
 CCWm=4
 
 #GPIOの設定
-GPIO.setmode(GPIO.BCM)              #GPIOのモードを"GPIO.BCM"に設定
-GPIO.setup(CWp, GPIO.OUT)          #GPIO27を出力モードに設定
-GPIO.setup(CWm, GPIO.OUT)          #GPIO27を出力モードに設定
-GPIO.setup(CCWp, GPIO.OUT)           #GPIO18を入力モードに設定
-GPIO.setup(CCWm, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)               #GPIOのモードを"GPIO.BCM"に設定
+GPIO.setup(CWp, GPIO.OUT)            #GPIO17を出力モードに設定
+GPIO.setup(CWm, GPIO.OUT)            #GPIO3を出力モードに設定
+GPIO.setup(CCWp, GPIO.OUT)           #GPIO23を出力モードに設定
+GPIO.setup(CCWm, GPIO.OUT)           #GPIO4を出力モードに設定
 
 while True:
   try:
     for i in range(1000):
       GPIO.output(CWp, GPIO.HIGH)
-      GPIO.output(CWm, GPIO.LOW)            #GPIO27の出力をHigh(3.3V)にする
+      GPIO.output(CWm, GPIO.LOW)             #CWをONに
       time.sleep(0.05)
       GPIO.output(CWp, GPIO.LOW)
-      GPIO.output(CWm, GPIO.HIGH)            #GPIO27の出力をHigh(3.3V)にする
+      GPIO.output(CWm, GPIO.HIGH)            #CWをOFFに
       time.sleep(0.05)
     for i in range(1000):
       GPIO.output(CCWp, GPIO.HIGH)
-      GPIO.output(CCWm, GPIO.LOW)            #GPIO27の出力をHigh(3.3V)にする
+      GPIO.output(CCWm, GPIO.LOW)            #CCWをONに
       time.sleep(0.05)
       GPIO.output(CCWp, GPIO.LOW)
-      GPIO.output(CCWm, GPIO.HIGH)            #GPIO27の出力をHigh(3.3V)にする
+      GPIO.output(CCWm, GPIO.HIGH)           #CCWをOFFに
       time.sleep(0.05)
     
   except KeyboardInterrupt:
