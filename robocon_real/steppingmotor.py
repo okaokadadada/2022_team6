b33cd2eb1aba0f88da542a6e220c2800c1951143
ordonese <108791046+ordonese@users.noreply.github.com>
2022-09-24 12:19:39 +0900

    Update steppingmotor.py

--- robocon_real/steppingmotor.py
+++ robocon_real/steppingmotor.py
@@ -15,7 +15,7 @@ GPIO.setup(CWm, GPIO.OUT)            #GPIO3を出力モードに設定
 GPIO.setup(CCWp, GPIO.OUT)           #GPIO23を出力モードに設定
 GPIO.setup(CCWm, GPIO.OUT)           #GPIO4を出力モードに設定
 
-time.sleep(0.05)
+time.sleep(0.15)
 
 while True:
   try:
