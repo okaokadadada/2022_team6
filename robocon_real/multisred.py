5ef41b5a8ad53be559fb099418803e3605d4b7c8
ordonese <108791046+ordonese@users.noreply.github.com>
2022-10-09 17:10:11 +0900

    Update multisred.py

--- robocon_real/multisred.py
+++ robocon_real/multisred.py
@@ -5,21 +5,20 @@ a=0
 
 def func1():
     global a
-    while True:
-        a = a + 1
-        print(a)
-        time.sleep(1)
+    a = a + 1
+    print(a)
+    time.sleep(1)
 
 
 def func2():
     global a
-    while True:
-        if a%5==0:
-            print("1ラップB")
-            time.sleep(1)
+    if a%5==0:
+        print("1ラップB")
+        time.sleep(1)
 
 if __name__ == "__main__":
-    thread_1 = threading.Thread(target=func1)
-    thread_2 = threading.Thread(target=func2)
-    thread_1.start()
-    thread_2.start()
+    while True:
+        thread_1 = threading.Thread(target=func1)
+        thread_2 = threading.Thread(target=func2)
+        thread_1.start()
+        thread_2.start()
