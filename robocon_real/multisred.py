f2b028e393bd0bc04cffe4504a8419b02851dbd8
ordonese <108791046+ordonese@users.noreply.github.com>
2022-10-09 17:02:53 +0900

    Update multisred.py

--- robocon_real/multisred.py
+++ robocon_real/multisred.py
@@ -17,7 +17,7 @@ def func2():
         time.sleep(1)
 
 if __name__ == "__main__":
-    thread_1 = threading.Thread(target=func1,b=1)
+    thread_1 = threading.Thread(target=func1)
     thread_2 = threading.Thread(target=func2)
     while True:
         thread_1.start()
