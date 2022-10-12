ac051db47dbb7c1992f3b156c8df90d0944183a9
ordonese <108791046+ordonese@users.noreply.github.com>
2022-10-12 13:20:05 +0900

    Update SupersonicSensor.py

--- robocon_real/SupersonicSensor.py
+++ robocon_real/SupersonicSensor.py
@@ -38,24 +38,16 @@ def read_distance():
     return distance
 
 #連続して値を超音波センサの状態を読み取る
-try:
-    if __name__ == "__main__":
-        thread_1 = threading.Thread(target=read_distance)
-        thread_1.start()
+while True:
+    try:
+        cm = -read_distance()
+        print(cm)                   #HC-SR04で距離を測定する
+        if cm > 2 and cm < 400:                #距離が2～400cmの場合
+            print("distance=", int(cm), "cm")  #距離をint型で表示
+        else:
+            print("over")
+        time.sleep(1)                          #1秒間待つ
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    sys.exit()  
-#while True:
-#    try:
-#        cm = -read_distance()
-#        print(cm)                   #HC-SR04で距離を測定する
-#        if cm > 2 and cm < 400:                #距離が2～400cmの場合
-#            print("distance=", int(cm), "cm")  #距離をint型で表示
-#        # else:
-#        #     print("over")
-#        time.sleep(1)                          #1秒間待つ
-
-#    except KeyboardInterrupt:       #Ctrl+Cキーが押された
-#        GPIO.cleanup()              #GPIOをクリーンアップ
-#        sys.exit()                  #プログラム終了
+    except KeyboardInterrupt:       #Ctrl+Cキーが押された
+        GPIO.cleanup()              #GPIOをクリーンアップ
+        sys.exit()                  #プログラム終了
