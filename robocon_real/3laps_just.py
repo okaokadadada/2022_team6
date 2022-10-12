975f8346ce2b0be2971a610107e19e601c945e31
ordonese <108791046+ordonese@users.noreply.github.com>
2022-10-12 13:08:37 +0900

    Update 3laps_just.py

--- robocon_real/3laps_just.py
+++ robocon_real/3laps_just.py
@@ -194,8 +194,8 @@ def mortor_L():
 if __name__ == "__main__":
     thread_1 = threading.Thread(target=read_distance)
     #thread_2 = threading.Thread(target=mortor_R)
-    thread_3 = threading.Thread(target=mortor_L)
+    #thread_3 = threading.Thread(target=mortor_L)
     
     thread_1.start()
     #thread_2.start()
-    thread_3.start()
+    #thread_3.start()
