0c13016ea8ebebd63b8996ecb102e05fb1ec5b40
ordonese <108791046+ordonese@users.noreply.github.com>
2022-10-12 19:37:42 +0900

    Update 3laps_just.py

--- robocon_real/3laps_just.py
+++ robocon_real/3laps_just.py
@@ -37,29 +37,31 @@ GPIO.setup(Echo_F, GPIO.IN)           #GPIO18を入力モードに設定
 GPIO.setup(Trig_L, GPIO.OUT)          #GPIO27を出力モードに設定
 GPIO.setup(Echo_L, GPIO.IN)           #GPIO18を入力モードに設定
 
+#距離測定に使用する変数
 a=0
-
+b=0
+c=0
+d=0
+sig_on_F = 0
+sig_off_F = 0
+sig_on_L = 0
+sig_off_L = 0
 duration_F = 0
 duration_L = 0
+distance_F = 0
+distance_L = 0
+distanceborder_F = 20
+distanceborder_L = 20
 
 #旋回回数
 turn = 0
 
-#前壁との距離を格納する変数
-distance_F = 0
-
-#左壁との距離を格納する変数
-distance_L = 0
-
-#旋回を始める距離
-distanceborder_F = 20
-
-#左の壁との最短距離
-distanceborder_L = 20
-
 #HC-SR04で距離を測定する関数
 def read_distance():
     global a
+    global b
+    global c
+    global d
     global sig_on_F
     global sig_off_F
     global sig_on_L
@@ -69,32 +71,58 @@ def read_distance():
     global distance_F
     global distance_L
     while True:
+        if a>300 or b>300 or c>300 or d>300:  #リセット報告
+            a=0
+            b=0
+            c=0
+            d=0
+            print("reset")
+        a=0
+        b=0
+        c=0
+        d=0
         #前方
         GPIO.output(Trig_F, GPIO.HIGH)            #GPIO27の出力をHigh(3.3V)にする
-        time.sleep(0.000015)                     #10μ秒間待つ
+        time.sleep(0.00001)                     #10μ秒間待つ
         GPIO.output(Trig_F, GPIO.LOW)             #GPIO27の出力をLow(0V)にする
 
         while GPIO.input(Echo_F) == GPIO.LOW:     #GPIO18がLowの時間
             sig_off_F = time.time()
-            print("A")
+            a=a+1
+            if a>300:
+                break
+        if a>300:
+            continue
         while GPIO.input(Echo_F) == GPIO.HIGH:    #GPIO18がHighの時間
             sig_on_F = time.time()
-            print("B")
+            b=b+1
+            if b>300:
+                break
+        if b>300:
+            continue
         duration_F = sig_on_F -sig_off_F            #GPIO18がHighしている時間を算術
         distance_F = duration_F * 34000 / 2         #距離を求める(cm)
-        time.sleep(0.1)
+        time.sleep(0.075)
 
         #左方
         GPIO.output(Trig_L, GPIO.HIGH)            #GPIO27の出力をHigh(3.3V)にする
-        time.sleep(0.000015)                     #10μ秒間待つ
+        time.sleep(0.00001)                     #10μ秒間待つ
         GPIO.output(Trig_L, GPIO.LOW)             #GPIO27の出力をLow(0V)にする
 
         while GPIO.input(Echo_L) == GPIO.LOW:     #GPIO18がLowの時間
             sig_off_L = time.time()
-            print("C")
+            c=c+1
+            if c>300:
+                break
+        if c>300:
+            continue
         while GPIO.input(Echo_L) == GPIO.HIGH:    #GPIO18がHighの時間
             sig_on_L = time.time()
-            print("D")
+            d=d+1
+            if d>300:
+                break
+        if d>300:
+            continue
         duration_L = sig_on_L - sig_off_L           #GPIO18がHighしている時間を算術
         distance_L = duration_L * 34000 / 2         #距離を求める(cm)
         time.sleep(0.1)
