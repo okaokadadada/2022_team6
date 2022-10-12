a2135547c0c1973696aa1050c929ab2195c44c6a
ordonese <108791046+ordonese@users.noreply.github.com>
2022-10-12 17:27:12 +0900

    Update SupersonicSensor.py

--- robocon_real/SupersonicSensor.py
+++ robocon_real/SupersonicSensor.py
@@ -11,8 +11,8 @@ Echo_F = 23
 Trig_L = 10
 Echo_L = 24
 
-time = 0.00001
-sleep = 0.05
+times = 0.00001
+sleeps = 0.05
 
 sig_on_F = 0
 sig_off_F = 0
@@ -42,7 +42,7 @@ def read_distance():
     global distance_L
 
     GPIO.output(Trig_F, GPIO.HIGH)            #GPIO27の出力をHigh(3.3V)にする
-    time.sleep(time)                     #10μ秒間待つ
+    time.sleep(times)                     #10μ秒間待つ
     GPIO.output(Trig_F, GPIO.LOW)             #GPIO27の出力をLow(0V)にする
 
     while GPIO.input(Echo_F) == GPIO.LOW:     #GPIO18がLowの時間
@@ -54,11 +54,11 @@ def read_distance():
 
     duration_F = sig_on_F - sig_off_F             #GPIO18がHighしている時間を算術
     distance_F = duration_F * 34000 / 2         #距離を求める(cm)
-    time.sleep(sleep)                          #1秒間待つ
+    time.sleep(sleeps)                          #1秒間待つ
     
     
     GPIO.output(Trig_L, GPIO.HIGH)            #GPIO27の出力をHigh(3.3V)にする
-    time.sleep(time)                     #10μ秒間待つ
+    time.sleep(times)                     #10μ秒間待つ
     GPIO.output(Trig_L, GPIO.LOW)             #GPIO27の出力をLow(0V)にする
 
     while GPIO.input(Echo_L) == GPIO.LOW:     #GPIO18がLowの時間
@@ -70,7 +70,7 @@ def read_distance():
 
     duration_L = sig_on_L - sig_off_L             #GPIO18がHighしている時間を算術
     distance_L = duration_L * 34000 / 2         #距離を求める(cm)
-    time.sleep(sleep)                          #1秒間待つ
+    time.sleep(sleeps)                          #1秒間待つ
 
 #連続して値を超音波センサの状態を読み取る
 while True:
