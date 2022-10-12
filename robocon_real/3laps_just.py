05a22386f0a4725e5df2efe7a67fdb3775a227fd
ordonese <108791046+ordonese@users.noreply.github.com>
2022-10-12 16:17:07 +0900

    Update 3laps_just.py

--- robocon_real/3laps_just.py
+++ robocon_real/3laps_just.py
@@ -80,13 +80,13 @@ def read_distance():
             #print("sig_off_F")
             a=a+1
         print("1=", a)
-        while GPIO.input(Echo_F) == GPIO.HIGH:    #GPIO18がHighの時間
-            sig_on_F = time.time()
-            #print("sig_on_F")
-            a=a+1
+        #while GPIO.input(Echo_F) == GPIO.HIGH:    #GPIO18がHighの時間
+        sig_on_F = time.time()
+        #print("sig_on_F")
+        a=a+1
         print("2=", a)
-        duration_F = sig_off_F - sig_on_F             #GPIO18がHighしている時間を算術
-        distance_F = -(duration_F * 34000 / 2)         #距離を求める(cm)
+        duration_F = sig_on_F -sig_off_F            #GPIO18がHighしている時間を算術
+        distance_F = duration_F * 34000 / 2         #距離を求める(cm)
 
         #左方
         GPIO.output(Trig_L, GPIO.HIGH)            #GPIO27の出力をHigh(3.3V)にする
@@ -98,13 +98,13 @@ def read_distance():
             a=a+1
             #print("sig_off_L")
         print("3=", a)
-        while GPIO.input(Echo_L) == GPIO.HIGH:    #GPIO18がHighの時間
-            sig_on_L = time.time()
-            #print("sig_on_L")
-            a=a+1
+        #while GPIO.input(Echo_L) == GPIO.HIGH:    #GPIO18がHighの時間
+        sig_on_L = time.time()
+        #print("sig_on_L")
+        a=a+1
         print("4=", a)
-        duration_L = sig_off_L - sig_on_L             #GPIO18がHighしている時間を算術
-        distance_L = -(duration_L * 34000 / 2)         #距離を求める(cm)
+        duration_L = sig_on_L - sig_off_L           #GPIO18がHighしている時間を算術
+        distance_L = duration_L * 34000 / 2         #距離を求める(cm)
         
         #duration_F, duration_L, sig_on_F, sig_on_L, sig_off_F, sig_off_L, 
         print("前=", distance_F, "  左=", distance_L)
