c078655639887b6d112a3c325617d30aaff09c8e
ordonese <108791046+ordonese@users.noreply.github.com>
2022-10-12 17:06:28 +0900

    Update SupersonicSensor.py

--- robocon_real/SupersonicSensor.py
+++ robocon_real/SupersonicSensor.py
@@ -69,7 +69,7 @@ while True:
         read_distance()
         #print("duration_F=", duration_F, "duration_L=", duration_L)                   #HC-SR04で距離を測定する      
         print("distance_F=", distance_F, "cm", "distance_L=", distance_F, "cm")  #距離をint型で表示
-        #time.sleep(1)                          #1秒間待つ
+        time.sleep(1)                          #1秒間待つ
 
     except KeyboardInterrupt:       #Ctrl+Cキーが押された
         GPIO.cleanup()              #GPIOをクリーンアップ
