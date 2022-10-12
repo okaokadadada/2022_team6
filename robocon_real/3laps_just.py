9820153ea28f37c669c5de07e68bc283083edd21
ordonese <108791046+ordonese@users.noreply.github.com>
2022-10-12 12:44:09 +0900

    Update 3laps_just.py

--- robocon_real/3laps_just.py
+++ robocon_real/3laps_just.py
@@ -37,17 +37,14 @@ GPIO.setup(Echo_F, GPIO.IN)           #GPIO18を入力モードに設定
 GPIO.setup(Trig_L, GPIO.OUT)          #GPIO27を出力モードに設定
 GPIO.setup(Echo_L, GPIO.IN)           #GPIO18を入力モードに設定
 
-duration_F = 0
-duration_L = 0
-
 #旋回回数
 turn = 0
 
 #前壁との距離を格納する変数
-distance_F = 0
+#distance_F = 0
 
 #左壁との距離を格納する変数
-distance_L = 0
+#distance_L = 0
 
 #旋回を始める距離
 distanceborder_F = 20
