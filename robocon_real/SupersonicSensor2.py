fec85113e00f31a144b220c90542f826692ecf9c
ordonese <108791046+ordonese@users.noreply.github.com>
2022-10-27 20:52:19 +0900

    Update SupersonicSensor2.py

--- robocon_real/SupersonicSensor2.py
+++ robocon_real/SupersonicSensor2.py
@@ -1,34 +1,121 @@
-import RPi.GPIO as GPIO
-import time
+from signal import SIG_UNBLOCK
+import RPi.GPIO as GPIO             #GPIO用のモジュールをインポート
+import time                         #時間制御用のモジュールをインポート
+import sys                          #sysモジュールをインポート
+import threading
 
-#測定環境温度
-TEMP = 20
+#超音波センサのピン設定
+Trig_F = 22
+Echo_F = 23
+Trig_L = 10
+Echo_L = 24
 
-#GPIO設定
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(10, GPIO.OUT)
-GPIO.setup(24, GPIO.IN)
+#超音波センサのピン設定
+GPIO.setup(Trig_F, GPIO.OUT)          #GPIO27を出力モードに設定
+GPIO.setup(Echo_F, GPIO.IN)           #GPIO18を入力モードに設定
+GPIO.setup(Trig_L, GPIO.OUT)          #GPIO27を出力モードに設定
+GPIO.setup(Echo_L, GPIO.IN)           #GPIO18を入力モードに設定
 
-#繰り返し
-while True:
-    
-    #トリガ信号出力
-    GPIO.output(10, GPIO.HIGH)
-    time.sleep(0.00001)
-    GPIO.output(10, GPIO.LOW)
-    
-    #返送HIGHレベル時間計測
-    while GPIO.input(24) == GPIO.LOW:
-        soff = time.time()    #LOWレベル終了時刻
-    
-    while GPIO.input(24) == GPIO.HIGH:
-        son = time.time()    #HIGHレベル終了時刻
-    
-    #時間から距離に変換(TEMPは測定環境温度)
-    clc = (son - soff) * (331.50 + (0.6 * TEMP)) / 2 * 100
-    
-    #画面に表示
-    print(clc, "cm")
-    
-    #一時停止
-    time.sleep(0.1)
+#距離測定に使用する変数，定数
+a=0
+b=0
+c=0
+d=0
+rimit = 600
+sig_on_F = 0
+sig_off_F = 0
+sig_on_L = 0
+sig_off_L = 0
+duration_F = 0
+duration_L = 0
+distance_F = 30
+distance_L = 30
+distanceborder_F = 20
+distanceborder_L = 20
+
+#HC-SR04で距離を測定する関数
+def read_distance():
+    global a
+    global b
+    global c
+    global d
+    global update
+    global rimit
+    global sig_on_F
+    global sig_off_F
+    global sig_on_L
+    global sig_off_L
+    global duration_F
+    global duration_L
+    global distance_F
+    global distance_L
+    while True:
+        if a>300 or b>300 or c>300 or d>300:  #リセット報告
+            a=0
+            b=0
+            c=0
+            d=0
+            print("reset")
+        a=0
+        b=0
+        c=0
+        d=0
+        #前方
+        GPIO.output(Trig_F, GPIO.HIGH)            #GPIO27の出力をHigh(3.3V)にする
+        time.sleep(0.00001)                     #10μ秒間待つ
+        GPIO.output(Trig_F, GPIO.LOW)             #GPIO27の出力をLow(0V)にする
+
+        while GPIO.input(Echo_F) == GPIO.LOW:     #GPIO18がLowの時間
+            sig_off_F = time.time()
+            a=a+1
+            if a>rimit:
+                break
+        if a>rimit:
+            continue
+        while GPIO.input(Echo_F) == GPIO.HIGH:    #GPIO18がHighの時間
+            sig_on_F = time.time()
+            b=b+1
+            if b>rimit:
+                break
+        if b>rimit:
+            continue
+        duration_F = sig_on_F -sig_off_F            #GPIO18がHighしている時間を算術
+        distance_F = duration_F * 34000 / 2         #距離を求める(cm)
+        time.sleep(0.05)
+
+        #左方
+        GPIO.output(Trig_L, GPIO.HIGH)            #GPIO27の出力をHigh(3.3V)にする
+        time.sleep(0.00001)                     #10μ秒間待つ
+        GPIO.output(Trig_L, GPIO.LOW)             #GPIO27の出力をLow(0V)にする
+
+        while GPIO.input(Echo_L) == GPIO.LOW:     #GPIO18がLowの時間
+            sig_off_L = time.time()
+            c=c+1
+            if c>rimit:
+                break
+        if c>rimit:
+            continue
+        while GPIO.input(Echo_L) == GPIO.HIGH:    #GPIO18がHighの時間
+            sig_on_L = time.time()
+            d=d+1
+            if d>300:
+                break
+        if d>rimit:
+            continue
+        duration_L = sig_on_L - sig_off_L           #GPIO18がHighしている時間を算術
+        distance_L = duration_L * 34000 / 2         #距離を求める(cm)
+        time.sleep(0.05)
+        
+        #duration_F, duration_L, sig_on_F, sig_on_L, sig_off_F, sig_off_L, 
+        update = 1
+        print("前=", distance_F, "  左=", distance_L)
+
+try:
+    if __name__ == "__main__":
+        thread_1 = threading.Thread(target=read_distance)
+
+        thread_1.start()
+
+except KeyboardInterrupt:       #Ctrl+Cキーが押された
+        GPIO.cleanup()              #GPIOをクリーンアップ
+        sys.exit()                  #プログラム終了
