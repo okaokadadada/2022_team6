6e146aa9f13082099e4d35f29ff772e2e05441c1
ordonese <108791046+ordonese@users.noreply.github.com>
2022-10-27 20:49:27 +0900

    Update steppingmotor.py

--- robocon_real/steppingmotor.py
+++ robocon_real/steppingmotor.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO             #GPIO用のモジュールをインポート
 import time                         #時間制御用のモジュールをインポート
 import sys                          #sysモジュールをインポート
 
+#モータのGPIO設定
 CWp_R=2
 CWm_R=3
 CCWp_R=4
@@ -12,50 +13,60 @@ CWm_L=18
 CCWp_L=17
 CCWm_L=27
 
-#GPIOの設定
+waittime_R = 0.01
+waittime_L = 0.01
+
+#モータのGPIO設定
 GPIO.setmode(GPIO.BCM)               #GPIOのモードを"GPIO.BCM"に設定
-GPIO.setup(CWp_R, GPIO.OUT)            #GPIO17を出力モードに設定
-GPIO.setup(CWm_R, GPIO.OUT)            #GPIO3を出力モードに設定
-GPIO.setup(CCWp_R, GPIO.OUT)           #GPIO23を出力モードに設定
-GPIO.setup(CCWm_R, GPIO.OUT)           #GPIO4を出力モードに設定
-GPIO.setup(CWp_L, GPIO.OUT)            #GPIO17を出力モードに設定
-GPIO.setup(CWm_L, GPIO.OUT)            #GPIO3を出力モードに設定
-GPIO.setup(CCWp_L, GPIO.OUT)           #GPIO23を出力モードに設定
-GPIO.setup(CCWm_L, GPIO.OUT)           #GPIO4を出力モードに設定
+GPIO.setup(CWp_R, GPIO.OUT)
+GPIO.setup(CWm_R, GPIO.OUT)
+GPIO.setup(CCWp_R, GPIO.OUT)
+GPIO.setup(CCWm_R, GPIO.OUT)
+GPIO.setup(CWp_L, GPIO.OUT)
+GPIO.setup(CWm_L, GPIO.OUT)
+GPIO.setup(CCWp_L, GPIO.OUT)
+GPIO.setup(CCWm_L, GPIO.OUT)
 
-a=0.0004
-
-while True:
-  try:
-    a=0
-    b=0
-    for i in range(2000):
-    #  GPIO.output(CWp_R, GPIO.HIGH)
-    #  GPIO.output(CWm_R, GPIO.LOW)            #CCWをONに
-      GPIO.output(CWp_L, GPIO.HIGH)
-      GPIO.output(CWm_L, GPIO.LOW)
-      time.sleep(0.0005)
-    #  GPIO.output(CWp_R, GPIO.LOW)
-    #  GPIO.output(CWm_R, GPIO.HIGH)           #CCWをOFFに
-      GPIO.output(CWp_L, GPIO.LOW)
-      GPIO.output(CWm_L, GPIO.HIGH)
-      time.sleep(0.0005)
-    #  a=a+1
-    #  print("a=",a)
-    for j in range(2000):
-    #  GPIO.output(CCWp_R, GPIO.HIGH)
-    #  GPIO.output(CCWm_R, GPIO.LOW)            #CCWをONに
-      GPIO.output(CCWp_L, GPIO.HIGH)
-      GPIO.output(CCWm_L, GPIO.LOW)
-      time.sleep(0.0005)
-    #  GPIO.output(CCWp_R, GPIO.LOW)
-    #  GPIO.output(CCWm_R, GPIO.HIGH)           #CCWをOFFに
-      GPIO.output(CCWp_L, GPIO.LOW)
-      GPIO.output(CCWm_L, GPIO.HIGH)
-      time.sleep(0.0005)
-    #  b=b+1
-    #  print("b=",b)
+def right_G(waittime):  #右ステッピングモータを正転させる関数
+  GPIO.output(CWp_R, GPIO.HIGH)
+  GPIO.output(CWm_R, GPIO.LOW)             #CWをONに
+  time.sleep(waittime_R)
+  GPIO.output(CWp_R, GPIO.LOW)
+  GPIO.output(CWm_R, GPIO.HIGH)            #CWをOFFに
+  time.sleep(waittime_R)
+  
+def right_B(waittime):  #右ステッピングモータを逆転させる関数
+  GPIO.output(CCWp_R, GPIO.HIGH)
+  GPIO.output(CCWm_R, GPIO.LOW)             #CCWをONに
+  time.sleep(waittime)
+  GPIO.output(CCWp_R, GPIO.LOW)
+  GPIO.output(CCWm_R, GPIO.HIGH)            #CCWをOFFに
+  time.sleep(waittime)
     
-  except KeyboardInterrupt:
-    GPIO.cleanup()
-    sys.exit()
+def left_G(waittime):   #左ステッピングモータを正転させる関数
+  GPIO.output(CWp_L, GPIO.HIGH)
+  GPIO.output(CWm_L, GPIO.LOW)             #CWをONに
+  time.sleep(waittime_L)
+  GPIO.output(CWp_L, GPIO.LOW)
+  GPIO.output(CWm_L, GPIO.HIGH)            #CWをOFFに
+  time.sleep(waittime_L)
+  
+def left_B(waittime):   #左ステッピングモータを逆転させる関数
+  GPIO.output(CCWp_L, GPIO.HIGH)
+  GPIO.output(CCWm_L, GPIO.LOW)             #CCWをONに
+  time.sleep(waittime)
+  GPIO.output(CCWp_L, GPIO.LOW)
+  GPIO.output(CCWm_L, GPIO.HIGH)            #CCWをOFFに
+  time.sleep(waittime)
+  
+try:
+    if __name__ == "__main__":
+        thread_2 = threading.Thread(target=right_G)
+        thread_3 = threading.Thread(target=left_G)
+        
+        thread_2.start()
+        thread_3.start()
+
+except KeyboardInterrupt:       #Ctrl+Cキーが押された
+        GPIO.cleanup()              #GPIOをクリーンアップ
+        sys.exit()                  #プログラム終了
