64f285638bb14f7fa746bb56d693afde5074747f
ordonese <108791046+ordonese@users.noreply.github.com>
2022-10-31 23:02:46 +0900

    Update steppingmotor2.py

--- robocon_real/steppingmotor2.py
+++ robocon_real/steppingmotor2.py
@@ -3,10 +3,10 @@ import RPi.GPIO as GPIO             #GPIO用のモジュールをインポート
 import time                         #時間制御用のモジュールをインポート
 import sys                          #sysモジュールをインポート
 
-CWp=12  #25  #0
-CWm=6  #8  #5
-CCWp=5  #7  #6
-CCWm=7  #1  #13
+CWp=16  #25  #0
+CWm=19  #8  #5
+CCWp=20  #7  #6
+CCWm=21  #1  #13
 
 #GPIOの設定
 GPIO.setmode(GPIO.BCM)               #GPIOのモードを"GPIO.BCM"に設定
