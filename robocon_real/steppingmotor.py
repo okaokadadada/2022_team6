a3f2983ef2b372547a4b36c744db8c9438976ebf
ordonese <108791046+ordonese@users.noreply.github.com>
2022-11-07 17:56:21 +0900

    Update steppingmotor.py

--- robocon_real/steppingmotor.py
+++ robocon_real/steppingmotor.py
@@ -33,19 +33,19 @@ GPIO.setup(CCWm_L, GPIO.OUT)
 
 def turn_R():
   for i in range(500,2000,2):
-    GPIO.output(CWp_R, GPIO.HIGH)
-    GPIO.output(CWm_R, GPIO.LOW)             #CWをONに
+    GPIO.output(CCWp_R, GPIO.HIGH)
+    GPIO.output(CCWm_R, GPIO.LOW)             #CWをONに
     time.sleep(waittime_R+difference+(starttime/(i+1)))
-    GPIO.output(CWp_R, GPIO.LOW)
-    GPIO.output(CWm_R, GPIO.HIGH)            #CWをOFFに
+    GPIO.output(CCWp_R, GPIO.LOW)
+    GPIO.output(CCWm_R, GPIO.HIGH)            #CWをOFFに
     time.sleep(waittime_R+difference+(starttime/(i+1)))
   while True:
     for i in range(100):
-      GPIO.output(CWp_R, GPIO.HIGH)
-      GPIO.output(CWm_R, GPIO.LOW)             #CWをONに
+      GPIO.output(CCWp_R, GPIO.HIGH)
+      GPIO.output(CCWm_R, GPIO.LOW)             #CWをONに
       time.sleep(waittime_R)#+difference)
-      GPIO.output(CWp_R, GPIO.LOW)
-      GPIO.output(CWm_R, GPIO.HIGH)            #CWをOFFに
+      GPIO.output(CCWp_R, GPIO.LOW)
+      GPIO.output(CCWm_R, GPIO.HIGH)            #CWをOFFに
       time.sleep(waittime_R)#+difference)
 
 def turn_L():
