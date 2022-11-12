dba58944ca3c4077a470ed1b8e98e58b32651a13
ordonese <108791046+ordonese@users.noreply.github.com>
2022-11-12 16:43:22 +0900

    Update Servomotor.py

--- robocon_real/Servomotor.py
+++ robocon_real/Servomotor.py
@@ -10,20 +10,21 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN, GPIO.OUT)
 servo = GPIO.PWM(PIN, FREQ)
 
-#init
 servo.start(0.0)
 
-for i in range(2):#初期の9.1から2.5まで動かす
-  for i in range(70):
-    servo.ChangeDutyCycle(9.5-0.1*i)
-    time.sleep(INTERVAL)
+servo.ChangeDutyCycle(6.0)
+
+#for i in range(2):#初期の9.1から2.5まで動かす
+ # for i in range(70):
+  #  servo.ChangeDutyCycle(9.5-0.1*i)
+  #  time.sleep(INTERVAL)
   
-  time.sleep(0.5)
+ # time.sleep(0.5)
 
-  for i in range(70):
-    servo.ChangeDutyCycle(2.5+0.1*i)
-    time.sleep(INTERVAL)
+ # for i in range(70):
+  #  servo.ChangeDutyCycle(2.5+0.1*i)
+  #  time.sleep(INTERVAL)
 
-  time.sleep(0.5)
+ # time.sleep(0.5)
 
 GPIO.cleanup()
