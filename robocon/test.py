799198afe25e0a885c9468b5ecefb43a0b2d27fb
ordonese <108791046+ordonese@users.noreply.github.com>
2022-11-18 15:36:26 +0900

    Update test.py

--- robocon/test.py
+++ robocon/test.py
@@ -1,7 +1,10 @@
 #連続して値を超音波センサの状態を読み取る
 while True:
     try:
-        print(1+1)
+        a = 0
+        while True:
+            print(a)
+            a = a + 1
     except KeyboardInterrupt:       #Ctrl+Cキーが押された
         GPIO.cleanup()              #GPIOをクリーンアップ
         sys.exit()                  #プログラム終了
