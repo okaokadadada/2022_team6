4087d2a71cd1ead24cdcb19abb82624e5c7ae8c3
ordonese <108791046+ordonese@users.noreply.github.com>
2022-11-18 15:37:29 +0900

    Update test.py

--- robocon/test.py
+++ robocon/test.py
@@ -5,6 +5,7 @@ while True:
         while True:
             print(a)
             a = a + 1
+            time.sleep(1)
     except KeyboardInterrupt:       #Ctrl+Cキーが押された
         GPIO.cleanup()              #GPIOをクリーンアップ
         sys.exit()                  #プログラム終了
