509da36035552f5020b5ddccc5e0fae8cb4077af
ordonese <108791046+ordonese@users.noreply.github.com>
2022-11-29 17:26:00 +0900

    Update 3laps_just_test2.py

--- robocon_real/3laps_just_test2.py
+++ robocon_real/3laps_just_test2.py
@@ -112,16 +112,16 @@ def read_distance():
     
     
     while True:
-        if a>rimit or b>rimit or c>rimit or d>rimit:
-            a=0
-            b=0
-            c=0
-            d=0
-            #print("reset")
-        a=0
-        b=0
-        c=0
-        d=0
+        #if a>rimit or b>rimit or c>rimit or d>rimit:
+         #   a=0
+         #   b=0
+         #   c=0
+         #   d=0
+         #   #print("reset")
+        #a=0
+        #b=0
+        #c=0
+        #d=0
         #前方
         GPIO.output(Trig_F, GPIO.HIGH)            #GPIO27の出力をHigh(3.3V)にする
         time.sleep(0.00001)                     #10μ秒間待つ
@@ -129,18 +129,18 @@ def read_distance():
 
         while GPIO.input(Echo_F) == GPIO.LOW:     #GPIO18がLowの時間
             sig_off_F = time.time()
-            a=a+1
-            if a>rimit:
-                break
-        if a>rimit:
-            continue
+            #a=a+1
+            #if a>rimit:
+            #    break
+        #if a>rimit:
+          #  continue
         while GPIO.input(Echo_F) == GPIO.HIGH:    #GPIO18がHighの時間
             sig_on_F = time.time()
-            b=b+1
-            if b>rimit:
-                break
-        if b>rimit:
-            continue
+            #b=b+1
+            #if b>rimit:
+             #   break
+        #if b>rimit:
+           # continue
         duration_F = sig_on_F -sig_off_F            #GPIO18がHighしている時間を算術
         distance_F = duration_F * 34000 / 2         #距離を求める(cm)
         if distance_F - distance_preF > 100:
@@ -156,18 +156,18 @@ def read_distance():
 
         while GPIO.input(Echo_L) == GPIO.LOW:     #GPIO18がLowの時間
             sig_off_L = time.time()
-            c=c+1
-            if c>rimit:
-                break
-        if c>rimit:
-            continue
+            #c=c+1
+            #if c>rimit:
+            #    break
+        #if c>rimit:
+            #continue
         while GPIO.input(Echo_L) == GPIO.HIGH:    #GPIO18がHighの時間
             sig_on_L = time.time()
-            d=d+1
-            if d>rimit:
-                break
-        if d>rimit:
-            continue
+            #d=d+1
+            #if d>rimit:
+            #    break
+        #if d>rimit:
+          #  continue
         duration_L = sig_on_L - sig_off_L           #GPIO18がHighしている時間を算術
         distance_L = duration_L * 34000 / 2         #距離を求める(cm)
         if distance_L - distance_preL > 100:
