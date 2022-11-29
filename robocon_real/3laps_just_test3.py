6537be7704f3b223b046ea1de097032ca176eec6
ordonese <108791046+ordonese@users.noreply.github.com>
2022-11-29 17:46:27 +0900

    Update 3laps_just_test3.py

--- robocon_real/3laps_just_test3.py
+++ robocon_real/3laps_just_test3.py
@@ -274,7 +274,7 @@ def mortor():
             #    break
             turn = True
             time.sleep(1)
-            turn_R(fast,270,10)
+            turn_R(fast,270,5)
             time.sleep(1)
             turn = False
             
