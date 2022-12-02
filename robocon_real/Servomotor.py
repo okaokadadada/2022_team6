aa0f295c8cb7988ea7f96d19f804ce4386afd906
ordonese <108791046+ordonese@users.noreply.github.com>
2022-12-02 21:11:42 +0900

    Update Servomotor.py

--- robocon_real/Servomotor.py
+++ robocon_real/Servomotor.py
@@ -18,7 +18,7 @@ servo.start(0.0)
 
 for i in range(2):#初期の9.1から3.0まで動かす
  for i in range(66):
-   servo.ChangeDutyCycle(7-0.1*i)
+   servo.ChangeDutyCycle(8-0.1*i)
    time.sleep(INTERVAL)
   
  time.sleep(0.5)
